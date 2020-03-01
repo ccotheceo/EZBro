a8f19d0227bcb044129b1a0912127dd0e43497e9
Chuck Onyekwelu <ccotheceo@gmail.com>
2020-03-01 00:15:31 -0600

    updated home page

--- ezbro/src/components/NavigationBar.js
+++ ezbro/src/components/NavigationBar.js
@@ -23,9 +23,9 @@ export const NavigationBar = () => (
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar.nav">
             <Nav className="ml-auto">
-                <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
+                {/* <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item> */}
                 <Nav.Item><Nav.Link href="/workouts">Workouts</Nav.Link></Nav.Item>
-                <Nav.Item><Nav.Link href="/excercises">Exercises</Nav.Link></Nav.Item>
+                <Nav.Item><Nav.Link href="/exercises">Exercises</Nav.Link></Nav.Item>
                 <Nav.Item><Nav.Link href="/nutrition">Nutrition</Nav.Link></Nav.Item>
                 <Nav.Item><Nav.Link href="/login">Login</Nav.Link></Nav.Item>
             </Nav>
