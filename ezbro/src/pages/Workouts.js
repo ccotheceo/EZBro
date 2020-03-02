0c7709993f0987ca6f43f537926d1962d05935c7
Chuck Onyekwelu <ccotheceo@gmail.com>
2020-03-02 07:42:39 -0600

    updated workouts page

--- ezbro/src/pages/Workouts.js
+++ ezbro/src/pages/Workouts.js
@@ -1,31 +1,55 @@
 import React from 'react'
+import { Container, Card, ListGroup, Row, Col, Image } from 'react-bootstrap';
 
 
 export const Workouts = () => (
         <div class="Whole-page">
+            {/* if signed in display this but if not display please sign in */}
             <div class="header">
-                <h1>EZBRO</h1>
+                <h1>Workouts</h1>
             </div>
-            <nav>
+            <Card>
+                <ListGroup varient="flush">
+                    <ListGroup.Item>
+                        <Container>
+                            <Row>
+                                <Col sm={4}>
+                                <Image src="holder.js/171x180" thumbnail />
 
-            </nav>
-            <div class="content">
-              <div class="workout-card">
-                  <div>
-                    <h3>Workouts</h3>
-                  </div>
-                  <div>
-                      <ul>
-                          <li>Workout #1</li>
-                          <li>Workout #2</li>
-                          <li>Workout #3</li>
-                      </ul>
-                  </div>
-              </div>
+                                </Col>
+                                <Col sm={8}>
+                                    <Row><h5>Workout #1 Name</h5></Row>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Container>
+                            <Row>
+                                <Col sm={4}>
+                                <Image src="holder.js/171x180" thumbnail />
 
-            </div>
-            <div class="footer">
+                                </Col>
+                                <Col sm={8}>
+                                    <Row><h5>Workout #2 Name</h5></Row>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Container>
+                            <Row>
+                                <Col sm={4}>
+                                <Image src="holder.js/171x180" thumbnail />
 
-            </div>
+                                </Col>
+                                <Col sm={8}>
+                                    <Row><h5>Workout #3 Name</h5></Row>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </ListGroup.Item>
+                </ListGroup>
+            </Card>
         </div>
     )
