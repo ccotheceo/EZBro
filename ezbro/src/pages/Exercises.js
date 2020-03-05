c0bbacfad99b4fa9e77416a25b6c99f7336f5319
Chuck Onyekwelu <ccotheceo@gmail.com>
2020-03-04 18:03:40 -0600

    accepte all incoming changes

--- ezbro/src/pages/Exercises.js
+++ ezbro/src/pages/Exercises.js
@@ -1,38 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Nav } from 'react-bootstrap';
+import ExerciseList from '../components/ExerciseList';
+import axios from 'axios';
+
+function Exercises() ç{
+    const [exerciseState, setExerciseState] = useState({
+        
+        excercises: []
+    });
+
+    useEffect(() => {
+        // For demonstration purposes, we mock an API call.
+        axios.get('https://wger.de/api/v2/exercise/?limit=700')
+    .then(res => {
+        setExerciseState(res);
+        for (let i = 0; i < res.data.results.length; i++){
+            const singleEntry = {}
+            let exercises = res.data.results[i].exercises
+            let exercisesName = res.data.results[i].name
+            let exercisesEquipment = res.data.results[i].equipmentgi
+            singleEntry.category = exercises;
+            singleEntry.name = exercisesName;
+            singleEntry.equipment = exercisesEquipment;
+}
+        console.log(res)
+        });
+      }, []);
 
 
-export const Exercises = () => (
+
+
+    return (
         <div class="Whole-page">
             <div class="header">
-                <h1>EZBRO</h1>
+                <h1>Exercises</h1>
             </div>
-            <nav>
+            <div>
+                <Nav variant="tabs" defaultActiveKey="link-arms">
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-arms" onClick={() => setExerciseState({ ...exerciseState, category: 8 })}>Arms</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-legs" onClick={() => alert("legs!")}>Legs</Nav.Link>
+                        {/* <Nav.Link eventKey="link-legs" onClick={() => setExerciseState({ ...exerciseState, category: 9 })}>Legs</Nav.Link> */}
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-abs" onClick={() => setExerciseState({ ...exerciseState, category: 10 })}>Abs</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-chest" onClick={() => setExerciseState({ ...exerciseState, category: 11 })}>Chest</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-back" onClick={() => setExerciseState({ ...exerciseState, category: 12 })}>Back</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-shoulders" onClick={() => setExerciseState({ ...exerciseState, category: 13 })}>Shoulders</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-calves" onClick={() => setExerciseState({ ...exerciseState, category: 14 })}>Calves</Nav.Link>
+                    </Nav.Item>
 
-            </nav>
-            <div class="content">
-              <div class="exercise-card">
-                  <div>
-                    <h3>Exersises</h3>
-                  </div>
-                  <div>
-                      <ul>
-                          <li>Exercise #1</li>
-                          <li>Exercise #2</li>
-                          <li>Exercise #3</li>
-                      </ul>
-                  </div>
-                  <div>
-                      <ul>
-                          <li>Your Exercise #1</li>
-                          <li> Your Exercise #2</li>
-                          <li>Your Exercise #3</li>
-                      </ul>
-                  </div>
-              </div>
+                </Nav>
+                <ExerciseList />
 
             </div>
-            <div class="footer">
 
-            </div>
         </div>
     )
+}
+
+export default Exercises;
